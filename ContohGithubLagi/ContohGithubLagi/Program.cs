318d4fcc40f4cbeeb8bf430fa89295b70dfd1d31
WilliamJiemesha <67468607+WilliamJiemesha@users.noreply.github.com>
2020-12-07 09:17:32 +0800

    ubah jadi hai

--- ContohGithubLagi/ContohGithubLagi/Program.cs
+++ ContohGithubLagi/ContohGithubLagi/Program.cs
@@ -6,7 +6,7 @@ namespace ContohGithubLagi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hai");
         }
     }
 }
